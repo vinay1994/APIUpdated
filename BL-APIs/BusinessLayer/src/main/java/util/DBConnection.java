f2e0ecd59807a563f102f25d558237d119921618
Satya Prakash Solanki <satya@Satyas-MacBook-Air.local>
2017-09-12 16:22:11 +0530

    Test cases added

---
+++ BL-APIs/BusinessLayer/src/main/java/util/DBConnection.java
@@ -0,0 +1,45 @@
+package util;
+
+import java.io.IOException;
+import java.sql.*;
+
+import com.jcraft.jsch.Session;
+public class DBConnection{
+	static Statement stmt;
+	static Connection con = null;
+	static Session session = null;
+	static int id=0,irs=0;
+
+	public static void CreateDBConnection()throws ClassNotFoundException, SQLException, IOException{
+		//CreateSSHTunnel();
+		String dbUrl = "jdbc:mysql://umsdbm.fliplearn.com:3306/";
+		String username = "ums_read";String password = "ums_read$$514";String DbName = "ums_api";
+		Class.forName("com.mysql.jdbc.Driver"); 
+		try {
+			con=DriverManager.getConnection(dbUrl+DbName,username,password);
+			stmt = con.createStatement();
+		} catch (Exception e) {}
+	}    
+
+	public static void disconnectDBConnection() throws ClassNotFoundException, SQLException
+	{
+		//session.disconnect();
+		try {
+			con.close();
+		} catch (Exception e) {}
+
+	}
+
+	public static ResultSet executeQuery(String query) throws SQLException
+	{  
+		ResultSet rs= stmt.executeQuery(query); 
+		return rs;
+	}
+
+	public static int executeUpdate(String query) throws SQLException
+	{   
+		int rs= stmt.executeUpdate(query);
+		return rs;	
+	}
+}
+
--- BL-APIs/BusinessLayer/src/test/java/flip/api/businesslayer/BusinessLayer/AppTest.java
+++ BL-APIs/BusinessLayer/src/test/java/flip/api/businesslayer/BusinessLayer/AppTest.java
@@ -1,38 +1,67 @@
 package flip.api.businesslayer.BusinessLayer;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import util.Xls_Reader;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+import com.jayway.restassured.response.Response;
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+public class AppTest extends App{
+	Xls_Reader xls;
+	@BeforeClass
+	public void initialize(){
+		xls=new Xls_Reader();
+	}
+	@Test(priority=1)
+	public void loginRequest() throws JSONException,InterruptedException {
+		sheetName="Login";
+		int rowCount=xls.getRowCount(sheetName);
+		for(int i=2;i<rowCount+1;i++){
+			APIUrl = xls.getCellData(sheetName, "API_URL", i);
+			APIBody = xls.getCellData(sheetName, "API_REQUEST", i).trim();
+			APIResponse=xls.getCellData(sheetName, "API_RESPONSE_Expected", i).trim();
+			Response response=hitPostRequest(APIUrl, APIBody);
+			int statusCode=response.getStatusCode();
+			JSONObject JSONResponseBody = new JSONObject(response.body().asString());
+			setResponseCodeWithResponseData(sheetName,i,statusCode,response.body().asString());
+			if(statusCode==200 && (!response.body().asString().contains(":40"))){
+				System.out.println("\nStatus Code is "+statusCode);
+				sessionToken=JSONResponseBody.getJSONObject("data").getString("sessionToken");
+				uuid=JSONResponseBody.getJSONObject("data").getString("uuid");
+				System.out.println("\nsessionToke is "+sessionToken);
+				Assert.assertEquals(uuid, xls.getCellData(sheetName, "UUID", 2));
+				xls.setCellData(sheetName, "RESULT", i, "Pass");
+			}else if(response.body().asString().contains(":40")){
+				errorMessage=JSONResponseBody.getJSONObject("error").getString("errorMessage");
+				errorCode=JSONResponseBody.getJSONObject("error").getInt("errorCode");
+				System.out.println("\nerror Code is "+errorCode);
+				System.out.println(errorMessage);
+				Assert.assertEquals(errorMessage, xls.getCellData(sheetName, "API_RESPONSE_Expected", i));
+				xls.setCellData(sheetName, "RESULT", i, "Pass");
+			}
+		}
+	}
+	@Test(priority=2)
+	public void getUserProfile() throws JSONException{
+		sheetName="getUserProfile";
+		int rowCount=xls.getRowCount(sheetName);
+		for(int i=2;i<rowCount+1;i++){
+			APIUrl=xls.getCellData(sheetName, "API_URL", i);
+			Response response=hitGetRequest(APIUrl);
+			int statusCode=response.getStatusCode();
+			setResponseCodeWithResponseData(sheetName,i, statusCode, response.body().asString());
+			JSONObject jsonObject = new JSONObject(response.body().asString());
+			printStatement("\nStatus Code is "+statusCode);
+			JSONArray jsonArray = jsonObject.getJSONArray("response");
+			profileCode = jsonArray.getJSONObject(0).getString("profileCode");
+			printStatement("\nProfile code is "+profileCode);
+			Assert.assertEquals(profileCode, xls.getCellData(sheetName, "profileCode", 2));
+			xls.setCellData(sheetName, "RESULT", i, "Pass");
+		}
+	}
 }
