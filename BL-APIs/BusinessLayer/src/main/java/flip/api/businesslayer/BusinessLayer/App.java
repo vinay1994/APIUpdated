4f019e31cc33d2f9c49c5d89eb73a03379264596
Satya Prakash Solanki <satya@Satyas-MacBook-Air.local>
2017-10-05 11:55:58 +0530

    Event class file added

--- BL-APIs/BusinessLayer/src/main/java/flip/api/businesslayer/BusinessLayer/App.java
+++ BL-APIs/BusinessLayer/src/main/java/flip/api/businesslayer/BusinessLayer/App.java
@@ -1,14 +1,30 @@
 package flip.api.businesslayer.BusinessLayer;
 
 import static com.jayway.restassured.RestAssured.given;
+
+import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.TimeZone;
+
 import org.testng.Assert;
+
 import util.DBConnection;
 import util.Xls_Reader;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.restassured.builder.RequestSpecBuilder;
 import com.jayway.restassured.response.Response;
 import com.jayway.restassured.specification.RequestSpecification;
+import com.model.Event;
+import com.model.Payload;
 
 public class App {
 	public static String loginId="";
@@ -32,7 +48,88 @@ public class App {
 
 	}
 
+	public String getRequestJson(int i, String sheetName, String eventType){
+		 Xls_Reader xls=new Xls_Reader();
+		 Event event = new Event();
+		 Payload payload = new Payload();
+		 String jsonStr = null;
+		 payload.setSchoolCode(xls.getCellData(sheetName, "school_code", i));
+		 payload.setAyid(xls.getCellData(sheetName, "ayid", i));
+		 payload.setGroupCodeList(new ArrayList<String>(Arrays.asList(xls.getCellData(sheetName, "groupCodeList", i).split(","))));
+		 payload.setMessageCode(xls.getCellData(sheetName, "message_code", i));
+		 payload.setUuidList(new ArrayList<String>(Arrays.asList(xls.getCellData(sheetName, "uuid", i).split(","))));
+		 payload.setSmsEnabled(xls.getCellData(sheetName, "sendsms", i).equals("1") ? true : false);
+//		 event.setEventTime(new Date("2017-09-12T17:58:43.511Z"));
 
+		 String s = "2017-10-04T22:58:00.000Z";
+		 DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX");
+		 Date d = null;
+		try {
+			d = formatter.parse(s);
+		} catch (ParseException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		 System.out.println("Formatted Date in current time zone = " + formatter.format(d));
+
+		 TimeZone tx=TimeZone.getTimeZone("Asia/Calcutta");
+		 formatter.setTimeZone(tx);
+		 System.out.println("Formatted date in IST = " + formatter.format(d));
+		 
+		 Instant instant = Instant.now();
+		 System.out.println(instant.toString());
+		 String in = instant.toString();
+		 
+		 event.setEventType("HOMEWORK_CREATE");
+		 event.setEventDate(in);
+		 ObjectMapper payloadMapper = new ObjectMapper();
+         String payloadJson = "";
+	        try {
+	            // get event object as a json string
+	             payloadJson= payloadMapper.writeValueAsString(payload);
+	            //System.out.println(payloadJson);
+	        } catch (IOException e) {
+	            // TODO Auto-generated catch block
+	            e.printStackTrace();
+	        }
+		 event.setPayload(payloadJson);
+		 ObjectMapper mapperObj = new ObjectMapper();
+         
+	        try {
+	            // get event object as a json string
+	            jsonStr = mapperObj.writeValueAsString(event);
+	            //System.out.println(jsonStr);
+	        } catch (IOException e) {
+	            // TODO Auto-generated catch block
+	            e.printStackTrace();
+	        }
+		return jsonStr;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	public Response hitPostRequest(String APIUrl,String APIBody){
+		Response response = null;
+		try {
+			RequestSpecBuilder builder = new RequestSpecBuilder();
+			builder.setBody(APIBody);			
+			builder.setContentType(Content_Type);			
+			RequestSpecification requestSpec = builder.build();
+			response = given()
+					.authentication().preemptive().basic("", "").spec(requestSpec).when().post(APIUrl);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return response;
+	}
+	
+	
 	public Response hitPostRequest(String loginid, String APIUrl,String APIBody){
 		Response response = null;
 		try {
