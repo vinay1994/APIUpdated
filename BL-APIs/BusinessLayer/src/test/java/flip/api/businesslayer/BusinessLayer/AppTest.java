f9ebd8af613e3f999de2f9ad0e75579bc547d457
SatyaPraksashSolanki <satyaprakash.solanki@fliplearn.com>
2018-01-05 13:55:56 +0530

    New data added

--- BL-APIs/BusinessLayer/src/test/java/flip/api/businesslayer/BusinessLayer/AppTest.java
+++ BL-APIs/BusinessLayer/src/test/java/flip/api/businesslayer/BusinessLayer/AppTest.java
@@ -1,24 +1,11 @@
 package flip.api.businesslayer.BusinessLayer;
-
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.logging.FileHandler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.Test;
-import org.testng.annotations.BeforeClass;
-
 import util.LoadProperty;
 import util.Xls_Reader;
-
 import com.jayway.restassured.response.Response;
 
 public class AppTest extends App{
@@ -34,7 +21,8 @@ public class AppTest extends App{
 	@AfterClass
 	public void CloseProcesses() throws ClassNotFoundException, SQLException{
 	}
-
+	
+	
 	@Test
 	public void executeAndVerifyResult(){
 		load=new LoadProperty();
@@ -42,7 +30,7 @@ public class AppTest extends App{
 		sheetName="ExecutionSheet";
 		int statusCode=200;
 		int rowCount=xls.getRowCount(sheetName);
-		//System.out.println("Row count"+rowCount);
+		System.out.println("Row count"+rowCount);
 		for(int i=2;i<=rowCount;i++){
 			loginId=(String) load.getProperty(xls.getCellData(sheetName, "RoleType", i));
 			getSessionToken(loginId);
@@ -105,59 +93,90 @@ public class AppTest extends App{
 	}
 
 	// Do not execute Below Function
-	public void createAnnouncement(){
-		sheetName="homework_create_uuid";
-		APIName="http://commevent.fliplearn.com:8084/event";
-		int rowCount=xls.getRowCount(sheetName);
-		for(int i=2;i<rowCount+1;i++){
-			APIBody= getRequestJson(i, sheetName, "HOMEWORK_CREATE");
-			printStatement(APIName);
-			printStatement(APIBody);
-			Response response=hitPostRequest(APIName, APIBody);
-			System.out.println("Response is-->>> "+response.asString());
-			int statusCode=response.getStatusCode();
-			printStatement("Status Code is "+statusCode);
-			xls.setCellData(sheetName, "Result", i, response.asString());
-		}
-	}
-
-
-	@Test
-	public void hitTopicIds(){
-		String Base_url="https://stgtoc.fliplearn.com:8087/";
-		String topicId,resourceId,resourceName,type,title,wouzaUrl;
-		int counter=2;
-		Response response = null;
-		JSONArray jsonArray = null;
-		JSONObject jsonObject;
-		xls=new Xls_Reader();
-		Logger logger;
-		try {  
-			FileHandler fh;
-			fh = new FileHandler("/Users/satya/Desktop/Logger/logger.txt");  
-			logger = Logger.getLogger("MyLog");  
-			logger.setUseParentHandlers(false);
-			logger.addHandler(fh);
-			SimpleFormatter formatter = new SimpleFormatter();  
-			fh.setFormatter(formatter);
-			for(int i=2;i<xls.getRowCount("topicId");i++){
-				topicId=xls.getCellData("topicId", "topic_Id", i);
-				System.out.println("\nRunning for topic id-->> "+topicId);
-				response = hitGetRequest("satya.staging", Base_url+"v1/resource?topicId="+topicId+"");
-				try {
-					jsonObject = new JSONObject(response.body().asString());
-					jsonArray = jsonObject.getJSONArray("response");
-				} catch (Exception e) {}
-
-				for(int j=0;j<jsonArray.length();j++){
-					//System.out.println(response.asString());	
-					logger.info("\n"+topicId+";"+jsonArray.getJSONObject(j).get("type").toString()+";"
-							+jsonArray.getJSONObject(j).get("title").toString()+";"+jsonArray.getJSONObject(j).get("wouzaUrl").toString());  							
-				}
-			}
-		}
-		catch(Exception e){
-			e.printStackTrace();
-		}
-	}
+//	public void createAnnouncement(){
+//		sheetName="homework_create_uuid";
+//		APIName="http://commevent.fliplearn.com:8084/event";
+//		int rowCount=xls.getRowCount(sheetName);
+//		for(int i=2;i<rowCount+1;i++){
+//			APIBody= getRequestJson(i, sheetName, "HOMEWORK_CREATE");
+//			printStatement(APIName);
+//			printStatement(APIBody);
+//			Response response=hitPostRequest(APIName, APIBody);
+//			System.out.println("Response is-->>> "+response.asString());
+//			int statusCode=response.getStatusCode();
+//			printStatement("Status Code is "+statusCode);
+//			xls.setCellData(sheetName, "Result", i, response.asString());
+//		}
+//	}
+//
+//
+//	@Test
+//	public void hitTopicIds(){
+//		String Base_url="https://stgtoc.fliplearn.com:8087/";
+//		String topicId,resourceId,resourceName,type,title,wouzaUrl;
+//		int counter=2;
+//		Response response = null;
+//		JSONArray jsonArray = null;
+//		JSONObject jsonObject;
+//		xls=new Xls_Reader();
+//		Logger logger;
+//		try {  
+//			FileHandler fh;
+//			fh = new FileHandler("/Users/satya/Desktop/Logger/logger.txt");  
+//			logger = Logger.getLogger("MyLog");  
+//			logger.setUseParentHandlers(false);
+//			logger.addHandler(fh);
+//			SimpleFormatter formatter = new SimpleFormatter();  
+//			fh.setFormatter(formatter);
+//			for(int i=2;i<xls.getRowCount("topicId");i++){
+//				topicId=xls.getCellData("topicId", "topic_Id", i);
+//				System.out.println("\nRunning for topic id-->> "+topicId);
+//				response = hitGetRequest("satya.staging", Base_url+"v1/resource?topicId="+topicId+"");
+//				try {
+//					jsonObject = new JSONObject(response.body().asString());
+//					jsonArray = jsonObject.getJSONArray("response");
+//				} catch (Exception e) {}
+//
+//				for(int j=0;j<jsonArray.length();j++){
+//					//System.out.println(response.asString());	
+//					logger.info("\n"+topicId+";"+jsonArray.getJSONObject(j).get("type").toString()+";"
+//							+jsonArray.getJSONObject(j).get("title").toString()+";"+jsonArray.getJSONObject(j).get("wouzaUrl").toString());  							
+//				}
+//			}
+//		}
+//		catch(Exception e){
+//			e.printStackTrace();
+//		}
+//	}
+//	@Test
+//	public void createAdmissionNumber(){
+//		load=new LoadProperty();
+//		xls=new Xls_Reader();
+//		sheetName="ExecutionSheet";
+//		int statusCode=200;
+//		int rowCount=xls.getRowCount(sheetName);
+//		//System.out.println("Row count"+rowCount);
+//		for(int i=2;i<=rowCount;i++){
+//			loginId=(String) load.getProperty(xls.getCellData(sheetName, "RoleType", i));
+//			//getSessionToken(loginId);
+//			//getProfileCode();
+//			APIName=xls.getCellData(sheetName, "API_NAME", i);
+//			printStatement("\n*********************************************");
+//			printStatement("Executing for "+APIName+"");
+//			printStatement("*********************************************");
+//			APIUrl = baseUlr + xls.getCellData(sheetName, "API_URL", i);
+//			APIBody = xls.getCellData(sheetName, "API_REQUEST", i).trim();
+//			APIMethod=xls.getCellData(sheetName, "API_METHOD", i).trim();
+//			if(APIMethod.equalsIgnoreCase("POST")){
+//				Response response=hitPostRequest(loginId,APIUrl, APIBody);
+//				try {
+//					statusCode=response.getStatusCode();
+//				} catch (Exception e) {}
+//				printStatement("Status Code is "+statusCode);
+//				printStatement(APIBody);
+//				printStatement(response.body().asString()+"\n");
+//				//setResponseCodeWithResponseData(sheetName,i, statusCode, response.body().asString());
+//			}
+//		}
+//	}
 }
